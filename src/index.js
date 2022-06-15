65e432f5f3532aa748fe177b5c1004cca3b62159
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-06-15 20:48:05 +0300

    Add handle to do code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target.new_task_description.value);
+  })
+
 });
+
+function handleToDo(todo) {
+
+  let li = document.createElement("li");
+  document.querySelector("#tasks").appendChild(li);
+
+  li.appendChild(document.createTextNode(todo));
+  var button = document.createElement("button");
+  button.setAttribute("id", "delete_todo");
+  button.textContent = ' x ';
+  li.appendChild(button);
+}
+
+
+function deleteToDo(todo) {
+
+}
